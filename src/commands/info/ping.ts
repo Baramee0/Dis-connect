4a5a0efefe9ff89b4101084b70ad14473b86f961
Baramee Chaiho <63894614+Baramee0@users.noreply.github.com>
2022-05-24 14:44:25 +0700

    Update ping.ts

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -4,6 +4,7 @@ export default new Command({
     name: "ping",
     description: "replies with pong",
     run: async ({ interaction }) => {
-        interaction.followUp("Pong3");
+        interaction.followUp("GoodJob");
     }
 });
+
